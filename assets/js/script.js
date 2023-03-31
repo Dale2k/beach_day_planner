98a9fe2cb64130311a7828d2c2f6fa30eccc0ab3
Darryl Subal <dsubal@att.net>
2023-03-31 14:38:37 -0400

    update html - css - js basic format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,30 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// Wrap code interacting with DOM in a call to jQuery to ensure
+// the code isn't run until it has finished rendering the html
+// elements
+
+$(document).ready(function () {
+  var headPara = $("#currentDay");
+  var todayDate = dayjs().format("dddd, MMMM D YYYY");
+  headPara.text(todayDate);
 });
+
+// variables
+
+var input9El = document.querySelector("#hour9");
+
+// var now = dayjs()
+// is essentially the same as calling new Date()
+// 24 clock used to compare time periods.
+var time = new Date().getHours();
+
+// add class (color of time box) based on time.
+
+if (time > 9) {
+  $("#hour9").addClass("past");
+} else if (time >= 9 && time < 10) {
+  $("#hour9").addClass("present");
+} else {
+  $("#hour9").addClass("future");
+}
+
+console.log(time);
